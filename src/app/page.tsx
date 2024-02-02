984b86f78d35f94d9b1e374b7384932ea516dcdf
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-02 06:05:15 +0000

    ✨ feat: Redesigned landing page with CTA buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Footer } from "~/components/footer";
 
-import { buttonVariants } from "~/components/ui/button";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
@@ -12,10 +13,10 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] from-zinc-200 via-orange-200 to-zinc-200">
       {/* NAVBAR */}
-      <div className="flex w-full items-center justify-between border-b border-border p-4">
-        <h1 className="text-2xl font-bold">Student Portal</h1>
+      <div className="flex w-full items-center justify-between px-4 py-3">
+        <h1 className="text-xl font-semibold">Student Portal</h1>
         <Link
           href="/auth/signin"
           className={`${buttonVariants()} rounded-full bg-blue-500 px-10 py-3 font-semibold text-white`}
@@ -26,13 +27,33 @@ export default async function Home() {
 
       {/* MAIN CONTENT */}
       <div className="container flex flex-1 flex-col items-center justify-center gap-12 px-4 py-16">
-        <div className="flex flex-col items-center gap-2">Hello World</div>
+        <div className="flex flex-col items-center">
+          <h1 className="text-5xl font-bold tracking-tight text-foreground">
+            Welcome to Student Portal
+          </h1>
+          <p className="mt-4 max-w-md text-balance text-center text-lg text-foreground">
+            This is a place where you can find info and enrol in any of the
+            courses we offer.
+          </p>
+          <div className="mt-8 flex gap-4">
+            <Link href={"/auth/signin"} className={buttonVariants()}>
+              Sign up
+            </Link>
+            <Link
+              href={"/"}
+              className={buttonVariants({
+                variant: "secondary",
+                className: "bg-white/40 hover:bg-white/60",
+              })}
+            >
+              Learn more
+            </Link>
+          </div>
+        </div>
       </div>
 
       {/* FOOTER */}
-      <div className="pb-2 text-sm">
-        <p>Made by Aryan.</p>
-      </div>
+      <Footer />
     </main>
   );
 }
