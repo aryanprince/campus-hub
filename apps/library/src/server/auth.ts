64a92786411529572507efd3620c94f8da0a7358
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-07 17:52:59 +0000

    🛂 feat (library): Implemented GitHub OAuth

--- apps/library/src/server/auth.ts
+++ apps/library/src/server/auth.ts
@@ -4,7 +4,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -47,14 +47,14 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GitHubProvider({
+      clientId: env.NEXTAUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.NEXTAUTH_GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
      *
-     * Most other providers require a bit more work than the Discord provider. For example, the
+     * Most other providers require a bit more work than the GitHub provider. For example, the
      * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
      * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
      *
