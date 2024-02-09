51b2b91b546af7cb1d3b5fa49c242d4f4066e426
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-09 15:04:22 +0000

    🛂 chore (student): Setup auth client session provider component in Root Layout

--- apps/student/src/app/layout.tsx
+++ apps/student/src/app/layout.tsx
@@ -6,30 +6,37 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import React from "react";
 
+import SessionProvider from "~/components/session-provider";
+import { getServerAuthSession } from "~/server/auth";
+
 export const metadata = {
   title: "Student Portal",
   description: "Student Portal Microservice, built by Aryan.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.className}`}>
-        <TRPCReactProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </TRPCReactProvider>
+        <SessionProvider session={session}>
+          <TRPCReactProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </TRPCReactProvider>
+        </SessionProvider>
       </body>
     </html>
   );
