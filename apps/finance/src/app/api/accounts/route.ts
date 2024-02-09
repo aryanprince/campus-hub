16630c1bded10502203e38a607a2109e4b2faf48
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-09 00:53:13 +0000

    ✨ feat (finance): Created POST endpoint to create a new finance account * Also added error checks if data exists or is invalid

--- apps/finance/src/app/api/accounts/route.ts
+++ apps/finance/src/app/api/accounts/route.ts
@@ -1,10 +1,87 @@
 import { db } from "../../../server/db";
 
-export async function GET(request: Request) {
+type RequestBody = {
+  studentId: string;
+};
+
+export async function GET() {
   const accounts = await db.financeAccount.findMany();
+
+  if (accounts.length === 0) {
+    return Response.json(
+      {
+        message: "No finance accounts exist",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
 
   return Response.json(
     { data: accounts },
+    {
+      status: 200,
+    },
+  );
+}
+
+export async function POST(request: Request) {
+  // Receive studentCardId and lectureId from request body
+  const { studentId } = (await request.json()) as RequestBody;
+
+  if (request.body === undefined) {
+    return Response.json(
+      { error: "Missing request body" },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  // If request body is missing parameters, throw error
+  if (!studentId) {
+    return Response.json(
+      { error: "Missing parameters in request body" },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const existingAccount = await db.financeAccount.findFirst({
+    where: {
+      studentId: studentId,
+    },
+  });
+
+  if (existingAccount) {
+    {
+      return Response.json(
+        {
+          message: "Finance account already exists",
+        },
+        { status: 400 },
+      );
+    }
+  }
+
+  const newAccount = await db.financeAccount.create({
+    data: {
+      studentId: studentId,
+      hasOutstandingBalance: false,
+    },
+    select: {
+      id: true,
+      studentId: true,
+      hasOutstandingBalance: true,
+    },
+  });
+
+  return Response.json(
+    {
+      data: newAccount,
+    },
     {
       status: 200,
     },
