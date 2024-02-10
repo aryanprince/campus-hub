1115ce368c6378d111c9d817a7a31b9ef1821aee
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-10 06:52:09 +0000

    ✨ feat (student): Built graduation page to view graduation eligibility

--- apps/student/src/app/dashboard/graduation/page.tsx
+++ apps/student/src/app/dashboard/graduation/page.tsx
@@ -1,7 +1,49 @@
+import { Badge } from "~/components/ui/badge";
+
 export default function Graduation() {
+  // TODO: Fetch graduation eligibility from the server, temporarily hardcoded
+  const eligible = false;
+
   return (
-    <div>
-      <h1>Graduation</h1>
+    <div className="flex flex-1 flex-col">
+      <div className="flex flex-col items-center justify-between">
+        {/* PAGE TITLE */}
+        <div className="flex w-full flex-col justify-start px-12 pt-6">
+          <h1 className="text-3xl font-semibold tracking-tight text-foreground">
+            Graduation
+          </h1>
+          <p className="text-muted-foreground">Graduation Eligibility</p>
+        </div>
+
+        {/* MAIN CONTENT */}
+        <div className="mt-44 flex max-w-md flex-1 flex-col items-center justify-center gap-8">
+          {eligible ? (
+            <div className="flex flex-col gap-8 rounded-md border p-8">
+              <div className="flex flex-col gap-6">
+                <Badge className="w-fit bg-emerald-900 text-emerald-400">
+                  Eligibility Success
+                </Badge>
+                <p>You are eligible to graduate. Congratulations!</p>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-8 rounded-md border p-8">
+              <div className="flex flex-col gap-6">
+                <Badge className="w-fit bg-red-900 text-red-300">
+                  Eligibility Failed
+                </Badge>
+                <div className="space-y-2">
+                  <p>You are currently not eligible to graduate.</p>
+                  <p>
+                    Please pay all due fees from the Finance Portal to be
+                    eligible to graduate.
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
