314ee68ba174a6c0c1d06cd4d358a2d18ff03907
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-10 10:08:42 +0000

    ✨ chore (student): Created utility function to generate student IDs

--- apps/student/src/lib/utils.ts
+++ apps/student/src/lib/utils.ts
@@ -8,3 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 export function getFirstName(fullName: string) {
   return fullName.split(" ")[0];
 }
+
+/**
+ * Utility function to generate a random student ID
+ *
+ * @returns A random 8-digit string, starting with "c"
+ */
+export function generateRandomStudentId() {
+  return `c${Math.random().toString().substring(2, 10)}`;
+}
