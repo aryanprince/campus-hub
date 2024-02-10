39c01c4a11f786a41fcaa19579f71e01ff5652e6
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-10 19:42:14 +0000

    🔧 build (student): Refactored to use shared tooling packages now

--- apps/student/next.config.js
+++ apps/student/next.config.js
@@ -5,6 +5,12 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    reactStrictMode: true,
+
+    /** We already do linting and typechecking as separate tasks in CI */
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
+};
 
 export default config;
