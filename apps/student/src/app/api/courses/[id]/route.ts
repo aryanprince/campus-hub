7af66b84940c3407ff5fe2775b89fd52d30b161f
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-10 06:20:53 +0000

    ✨ feat (student): Built GET endpoint to fetch course info by ID

---
+++ apps/student/src/app/api/courses/[id]/route.ts
@@ -0,0 +1,15 @@
+import { eq } from "drizzle-orm";
+import { course } from "~/server/db/schema";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: number } },
+) {
+  const id = params.id;
+
+  const currentCourse = await db?.query.course.findFirst({
+    where: eq(course.id, id),
+  });
+
+  return Response.json(currentCourse, { status: 200 });
+}
