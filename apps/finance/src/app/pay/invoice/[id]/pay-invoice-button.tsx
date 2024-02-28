632d632c1529fa1e278a69f161fa169592fd3140
Aryan Prince <aryanprincesanty@icloud.com>
2024-02-28 02:15:10 +0000

    ✨ feat (finance): Added dialog box to confirm payment

--- apps/finance/src/app/pay/invoice/[id]/pay-invoice-button.tsx
+++ apps/finance/src/app/pay/invoice/[id]/pay-invoice-button.tsx
@@ -1,27 +1,68 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { CreditCard } from "lucide-react";
 import { toast } from "sonner";
 
-import type { Invoice } from "~/server/db/schema";
+import type { invoice } from "~/server/db/schema";
 import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 
-type Invoice = typeof Invoice.$inferSelect;
+type Invoice = typeof invoice.$inferSelect;
 
 export function PayInvoiceButton({ invoice }: { invoice: Invoice }) {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Button
-      onClick={async () => {
-        await fetch(`/api/invoices/reference/${invoice.referenceId}/pay`, {
-          method: "PUT",
-        });
-        toast.message(`✅ Invoice paid successfully!`, {
-          description: `Reference ID: ${invoice.referenceId}`,
-        });
-      }}
-    >
-      <CreditCard className="mr-2 size-4" />
-      Pay
-    </Button>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button>
+          <CreditCard className="mr-2 size-4" />
+          Pay
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Enter amount to pay for invoice</DialogTitle>
+          <DialogDescription>
+            Provide an amount to pay, or select the full amount to pay the
+            invoice.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button
+            onClick={async () => {
+              try {
+                await fetch(
+                  `/api/invoices/reference/${invoice.referenceId}/pay`,
+                  {
+                    method: "PUT",
+                  },
+                );
+                router.refresh();
+                setIsOpen(false);
+                toast.message(`✅ Invoice paid successfully!`, {
+                  description: `Reference ID: ${invoice.referenceId}`,
+                });
+              } catch (error) {
+                toast.error(`❌ Error: Failed to pay invoice!`);
+              }
+            }}
+          >
+            Pay in Full
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
