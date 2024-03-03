126a528e89e3111dfbb3d75baa4fb957692b1762
Aryan Prince <aryanprincesanty@icloud.com>
2024-03-03 02:55:05 +0000

    🛂 chore (library): Initailized auth

---
+++ apps/library/src/server/auth.ts
@@ -0,0 +1,25 @@
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { Lucia } from "lucia";
+
+import { session, user } from "~/server/db/schema";
+
+const adapter = new DrizzlePostgreSQLAdapter(db, session, user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    // this sets cookies with super long expiration
+    // since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
+    expires: false,
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+// IMPORTANT!
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+  }
+}
