5c7747b72dc322772296487f401cce7b90a6fd80
Aryan Prince <aryanprincesanty@icloud.com>
2024-03-05 19:12:51 +0000

    📱 feat (library): Updated books grid and cards for mobile

--- apps/library/src/app/(app)/dashboard/books/page.tsx
+++ apps/library/src/app/(app)/dashboard/books/page.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import type { book } from "~/server/db/schema/main-schema";
+import type { Book } from "~/server/db/schema/main-schema";
 import { AspectRatio } from "~/components/ui/aspect-ratio";
 import { db } from "~/server/db/index";
-
-type Book = typeof book.$inferSelect;
 
 export default async function BooksPage() {
   const books = await db.query.book.findMany();
 
   return (
-    <div className="flex w-full flex-col gap-4 p-6 pt-0 md:p-8">
+    <div className="flex w-full flex-col gap-4 p-4 pt-0 md:p-8">
+      {/* PAGE TITLE */}
       <div>
-        <h1 className="text-2xl font-semibold md:text-4xl">Books</h1>
+        <h1 className="text-2xl font-semibold tracking-tight md:text-4xl">
+          All Books
+        </h1>
         <p className="text-sm text-muted-foreground md:text-base">
           A collection of books from the library. You can borrow books from
           here.
         </p>
       </div>
-      <div className="flex h-fit max-w-6xl flex-wrap items-start justify-evenly gap-4 pb-8 lg:justify-start">
+
+      {/* BOOK GRID - Maps over the BookCard component */}
+      <div className="grid h-fit max-w-6xl auto-cols-max grid-flow-row grid-cols-2 place-content-center gap-4 pb-4 md:grid-cols-3 lg:grid-cols-4">
         {books.map((book) => (
           <BookCard key={book.bookId} book={book} />
         ))}
@@ -32,9 +35,10 @@ function BookCard({ book }: { book: Book }) {
   return (
     <Link
       href={`/dashboard/books/${book.bookId}`}
-      className="flex h-fit w-[150px] flex-col gap-2 rounded-md border bg-card p-4 transition-colors hover:scale-[1.005] hover:border-neutral-300 dark:hover:border-neutral-600 md:w-[200px] md:p-6"
+      className="flex flex-col gap-2 rounded-sm border bg-card p-2 transition-colors hover:scale-[1.005] hover:border-neutral-300 dark:hover:border-neutral-600 md:p-2 lg:p-4"
       key={book.bookId}
     >
+      {/* BOOK IMAGE */}
       {book.image && book.title && (
         <AspectRatio ratio={1 / 1.6} className="bg-muted">
           <Image
@@ -45,8 +49,10 @@ function BookCard({ book }: { book: Book }) {
           />
         </AspectRatio>
       )}
+
+      {/* BOOK DETAILS */}
       <div>
-        <h1 className="truncate text-base font-medium md:text-lg">
+        <h1 className="truncate text-sm font-medium md:text-base">
           {book.title}
         </h1>
         <p className="truncate text-xs text-muted-foreground md:text-sm">
