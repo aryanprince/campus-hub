677202a398945b8566c484c508578df870bc6327
Aryan Prince <aryanprincesanty@icloud.com>
2024-03-05 06:09:56 +0000

    📱 chore (library): Improved grid layout on mobile

--- apps/library/src/app/(app)/dashboard/books/page.tsx
+++ apps/library/src/app/(app)/dashboard/books/page.tsx
@@ -11,7 +11,7 @@ export default async function BooksPage() {
   const books = await db.query.book.findMany();
 
   return (
-    <div className="flex flex-col gap-4 p-8">
+    <div className="flex w-full flex-col gap-4 p-8">
       <div>
         <h1 className="text-4xl font-semibold">Books</h1>
         <p className="text-muted-foreground">
@@ -19,7 +19,7 @@ export default async function BooksPage() {
           here.
         </p>
       </div>
-      <div className="flex h-fit max-w-6xl flex-wrap items-start gap-4 pb-8">
+      <div className="flex h-fit max-w-6xl flex-wrap items-start justify-evenly gap-4 pb-8 lg:justify-start">
         {books.map((book) => (
           <BookCard key={book.bookId} book={book} />
         ))}
@@ -32,7 +32,7 @@ function BookCard({ book }: { book: Book }) {
   return (
     <Link
       href={`/dashboard/books/${book.bookId}`}
-      className="flex h-fit w-[200px] flex-col gap-2 rounded-md border bg-card p-6 transition-colors hover:scale-[1.005] hover:border-neutral-300 dark:hover:border-neutral-600"
+      className="flex h-fit w-[150px] flex-col gap-2 rounded-md border bg-card p-6 transition-colors hover:scale-[1.005] hover:border-neutral-300 dark:hover:border-neutral-600 md:w-[200px]"
       key={book.bookId}
     >
       {book.image && book.title && (
@@ -47,7 +47,7 @@ function BookCard({ book }: { book: Book }) {
       )}
       <div>
         <h1 className="truncate text-lg font-medium">{book.title}</h1>
-        <p className="text-sm text-muted-foreground">{book.author}</p>
+        <p className="truncate text-sm text-muted-foreground">{book.author}</p>
       </div>
     </Link>
   );
--- apps/library/src/app/(app)/dashboard/layout.tsx
+++ apps/library/src/app/(app)/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./sidebar";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto flex min-h-screen max-w-screen-xl">
-      <div className="fixed flex h-full w-[275px] flex-1 shrink-0">
+      <div className="fixed hidden h-full w-[275px] flex-1 shrink-0 md:flex">
         <Sidebar />
       </div>
       <div className="flex h-screen flex-1 overflow-auto bg-background md:ml-[275px]">
