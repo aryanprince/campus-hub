0f5ae97b67938f0f05ac1d465fd2a7bd3d010338
Aryan Prince <aryanprincesanty@icloud.com>
2024-03-05 00:52:17 +0000

    ✨ feat (library): Created cards to display books

--- apps/library/next.config.js
+++ apps/library/next.config.js
@@ -11,6 +11,17 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default config;
--- apps/library/src/app/(app)/dashboard/books/page.tsx
+++ apps/library/src/app/(app)/dashboard/books/page.tsx
@@ -1,16 +1,36 @@
+import Image from "next/image";
+
+import type { book } from "~/server/db/schema/main-schema";
 import { db } from "~/server/db/index";
+
+type Book = typeof book.$inferSelect;
 
 export default async function BooksPage() {
   const books = await db.query.book.findMany();
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-semibold">Books page</h1>
-      <ul className="mt-4">
+      <h1 className="text-2xl font-semibold">Books</h1>
+      <div className="mt-4 flex gap-4">
         {books.map((book) => (
-          <li key={book.bookId}>{book.title}</li>
+          <BookCard key={book.bookId} book={book} />
         ))}
-      </ul>
+      </div>
+    </div>
+  );
+}
+
+function BookCard({ book }: { book: Book }) {
+  return (
+    <div
+      className="flex max-w-[200px] flex-col gap-2 rounded-md bg-neutral-50 p-6"
+      key={book.bookId}
+    >
+      <Image src={book.image} width={150} height={250} />
+      <div>
+        <h1 className="truncate text-lg font-medium">{book.title}</h1>
+        <p className="text-sm text-muted-foreground">{book.author}</p>
+      </div>
     </div>
   );
 }
