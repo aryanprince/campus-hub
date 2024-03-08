6b4b3025410590281069511a02f4ded92f5ebfac
Aryan Prince <aryanprincesanty@icloud.com>
2024-03-08 02:53:03 +0000

    🛂 feat (student): Setup Lucia auth, generated auth schema migrations

--- apps/student/src/server/db/schema.ts
+++ apps/student/src/server/db/schema.ts
@@ -4,6 +4,7 @@ import {
   primaryKey,
   serial,
   text,
+  timestamp,
   uniqueIndex,
 } from "drizzle-orm/pg-core";
 
@@ -38,3 +39,24 @@ export const enrollment = pgTable(
     };
   },
 );
+
+// =======================================================================
+// AUTH TABLES
+// =======================================================================
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  username: text("username").unique(),
+  hashedPassword: text("hashed_password"),
+});
+
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
