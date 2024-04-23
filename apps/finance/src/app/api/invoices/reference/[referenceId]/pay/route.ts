dc0e16bc7d61dd3003e92125d9bb3c3aa660fc15
Aryan Prince <aryanprincesanty@icloud.com>
2024-04-23 17:45:33 +0300

    🥅 feat (finance): Update graduation eligibility on invoice payment

--- apps/finance/src/app/api/invoices/reference/[referenceId]/pay/route.ts
+++ apps/finance/src/app/api/invoices/reference/[referenceId]/pay/route.ts
@@ -1,7 +1,7 @@
 import { eq } from "drizzle-orm";
 
 import { db } from "~/server/db";
-import { invoice } from "~/server/db/schema/main-schema";
+import { financeAccount, invoice } from "~/server/db/schema/main-schema";
 
 export async function PUT(
   request: Request,
@@ -9,22 +9,52 @@ export async function PUT(
 ) {
   const referenceId = params.referenceId;
 
-  const paidInvoice = await db
-    .update(invoice)
-    .set({
-      invoiceStatus: "PAID",
-    })
-    .where(eq(invoice.referenceId, referenceId))
-    .returning();
+  const paidInvoice = await db.transaction(async (tx) => {
+    // Update the invoice status to "PAID" in the database
+    const invoiceToBePaid = await tx
+      .update(invoice)
+      .set({
+        invoiceStatus: "PAID",
+      })
+      .where(eq(invoice.referenceId, referenceId))
+      .returning();
 
-  console.log(paidInvoice);
+    console.log(invoiceToBePaid);
 
-  if (!paidInvoice || paidInvoice.length < 1) {
-    return Response.json({
-      error: `Error! Invoice does not exist (Reference ID: ${referenceId})`,
-    });
-  }
+    // Check if the invoice exists, and return an error if it does not
+    if (!invoiceToBePaid || invoiceToBePaid.length < 1) {
+      return Response.json({
+        error: `Error! Invoice does not exist (Reference ID: ${referenceId})`,
+      });
+    }
 
+    // Check if the invoice has already been paid, and return an error if it has
+    if (invoiceToBePaid[0]?.invoiceStatus === "PAID") {
+      // Check if all invoices have been paid for the student
+      const allInvoicesForStudent = await tx.query.invoice.findMany({
+        where: eq(invoice.studentId, invoiceToBePaid[0].studentId),
+      });
+
+      // Boolean flag indicating if all invoices have been paid (true if all invoices have been paid, false otherwise)
+      const allInvoicesPaid = allInvoicesForStudent.every(
+        (invoice) => invoice.invoiceStatus === "PAID",
+      );
+
+      // Update student graduation eligibility based on invoice payment status
+      if (allInvoicesPaid) {
+        await tx
+          .update(financeAccount)
+          .set({
+            hasOutstandingBalance: false,
+          })
+          .where(eq(financeAccount.studentId, invoiceToBePaid[0].studentId));
+      }
+    }
+
+    return invoiceToBePaid;
+  });
+
+  // Return a success message if the invoice was successfully paid
   return Response.json(
     {
       message: `Successfully paid invoice (Reference ID: ${referenceId})`,
