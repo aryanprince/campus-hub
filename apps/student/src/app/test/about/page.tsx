77ce3352a5894cf3de149386054dfbb65fb25c73
Aryan Prince <aryanprincesanty@icloud.com>
2024-04-23 13:28:45 +0300

    ✨ feat (student): Integrate Playwright for E2E tests

--- apps/student/src/app/test/about/page.tsx
+++ apps/student/src/app/test/about/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>Home</h1>
-      <Link href="/test/about">About</Link>
+      <h1>About</h1>
+      <Link href="/">Home</Link>
     </div>
   );
 }
