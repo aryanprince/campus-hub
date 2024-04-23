d8d1d5165b02dbaa737cb692ca56dc64c3217432
Aryan Prince <aryanprincesanty@icloud.com>
2024-04-23 17:28:39 +0300

    🥅 feat (finance): Update eligibility status on invoice creation

--- apps/finance/src/app/api/invoices/route.ts
+++ apps/finance/src/app/api/invoices/route.ts
@@ -1,8 +1,8 @@
-import { asc } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 
 import { generateRandomReferenceString } from "~/lib/utils";
 import { db } from "~/server/db";
-import { invoice } from "~/server/db/schema/main-schema";
+import { financeAccount, invoice } from "~/server/db/schema/main-schema";
 
 type RequestBody = typeof invoice.$inferInsert;
 
@@ -17,17 +17,41 @@ export async function GET(_request: Request) {
 export async function POST(request: Request) {
   const requestBody = (await request.json()) as RequestBody;
 
-  const newInvoice = await db
-    .insert(invoice)
-    .values({
-      referenceId: generateRandomReferenceString(),
-      studentId: requestBody.studentId,
-      amount: requestBody.amount,
-      invoiceType: requestBody.invoiceType,
-      dueDate: requestBody.dueDate,
-      updatedAt: new Date(),
-    })
-    .returning();
+  try {
+    const newInvoice = await db.transaction(async (tx) => {
+      // Create a new invoice in the database
+      const newlyCreatedInvoice = await tx
+        .insert(invoice)
+        .values({
+          referenceId: generateRandomReferenceString(),
+          studentId: requestBody.studentId,
+          amount: requestBody.amount,
+          invoiceType: requestBody.invoiceType,
+          dueDate: requestBody.dueDate,
+          updatedAt: new Date(),
+        })
+        .returning();
 
-  return Response.json({ data: newInvoice }, { status: 200 });
+      // Update the student graduation eligibility status in the finance account
+      await tx
+        .update(financeAccount)
+        .set({
+          hasOutstandingBalance: true,
+        })
+        .where(eq(financeAccount.studentId, requestBody.studentId));
+
+      return newlyCreatedInvoice;
+    });
+
+    // Return the newly created invoice
+    return Response.json({ data: newInvoice }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+
+    // Return a 500 error if the transaction fails
+    return Response.json(
+      { error: "ERROR: Failed to create invoice" },
+      { status: 500 },
+    );
+  }
 }
