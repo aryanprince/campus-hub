5903349819cea75b8ef12741bb419b1c9637612c
Aryan Prince <aryanprincesanty@icloud.com>
2024-04-24 14:25:55 +0300

    ➖ chore (library): Remove Million Lint compiler

--- apps/library/next.config.js
+++ apps/library/next.config.js
@@ -1,5 +1,3 @@
-import MillionLint from "@million/lint";
-
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -9,13 +7,11 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+
   /** We already do linting and typechecking as separate tasks in CI */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
   images: {
     remotePatterns: [
       {
@@ -33,6 +29,5 @@ const config = {
     ],
   },
 };
-export default MillionLint.next({
-  rsc: true,
-})(config);
+
+export default config;
