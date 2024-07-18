4127e919e40b6477c97c83e2345d2bf036be29b8
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-18 11:22:16 +0100

    Runs local dev server before running tests

--- apps/student/playwright.config.ts
+++ apps/student/playwright.config.ts
@@ -6,6 +6,8 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
+const studentAppBaseUrl = "http://127.0.0.1:3001";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: studentAppBaseUrl,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -74,9 +76,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm dev",
+    url: studentAppBaseUrl,
+    reuseExistingServer: !process.env.CI,
+  },
 });
