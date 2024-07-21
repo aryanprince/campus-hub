e3845eb39cf1d99f418ae5043bf3eda72683166b
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-21 12:48:59 +0100

    ✅ test: Add test to create new user on Student

--- apps/student/tests/e2e-tests/auth/auth.setup.ts
+++ apps/student/tests/e2e-tests/auth/auth.setup.ts
@@ -1,25 +1,25 @@
-// import { faker } from "@faker-js/faker";
-// import { expect, test } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+import { expect, test } from "@playwright/test";
 
-// const authFile = "playwright/.auth/user.json";
+const authFile = "playwright/.auth/user.json";
 
-// test("should create a random new user", async ({ page }) => {
-//   const firstName = faker.person.firstName();
-//   const lastName = faker.person.lastName();
-//   const username = faker.internet.userName();
-//   const email = faker.internet.email();
-//   const password = faker.internet.password();
+test("should create a random new user", async ({ page }) => {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const username = faker.internet.userName();
+  const email = faker.internet.email();
+  const password = faker.internet.password();
 
-//   await page.goto("http://localhost:3001/signup");
-//   await page.getByLabel("First Name").fill(firstName);
-//   await page.getByLabel("Last Name").fill(lastName);
-//   await page.getByLabel("Username").fill(username);
-//   await page.getByLabel("Email").fill(email);
-//   await page.getByLabel("Password").fill(password);
-//   await page.getByText("Sign Up").click();
+  await page.goto("/signup");
+  await page.getByLabel("First Name").fill(firstName);
+  await page.getByLabel("Last Name").fill(lastName);
+  await page.getByLabel("Username").fill(username);
+  await page.getByLabel("Email").fill(email);
+  await page.getByLabel("Password").fill(password);
+  await page.getByText("Sign Up").click();
 
-//   await expect(page.getByText("Good morning")).toBeVisible();
+  await expect(page.getByText("Good morning")).toBeVisible();
 
-//   // End of authentication steps.
-//   await page.context().storageState({ path: authFile });
-// });
+  // End of authentication steps.
+  await page.context().storageState({ path: authFile });
+});
