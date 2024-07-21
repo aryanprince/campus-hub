865ebca7148da42ff32b7361ea6b254a0caa8d6a
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-21 20:19:01 +0100

    👷 ci: Update path to `HTML` report, fix PR comment typo

--- apps/student/playwright.config.ts
+++ apps/student/playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["json", { outputFile: "../../results.json" }]],
+  reporter: [
+    ["json", { outputFile: "../../results.json" }],
+    ["html", { outputFolder: "../../playwright-report/" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
