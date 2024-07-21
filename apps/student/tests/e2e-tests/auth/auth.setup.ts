977efdb3042d86b974d9aacc2cdf10824e73ba8d
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-21 21:14:02 +0100

    ⚰️ chore(student): Remove slow test timeout

--- apps/student/tests/e2e-tests/auth/auth.setup.ts
+++ apps/student/tests/e2e-tests/auth/auth.setup.ts
@@ -4,8 +4,6 @@ import { expect, test } from "@playwright/test";
 const authFile = "playwright/.auth/user.json";
 
 test("should create a random new user", async ({ page }) => {
-  test.slow();
-
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
   const username = faker.internet.userName();
