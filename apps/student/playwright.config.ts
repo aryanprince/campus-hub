29fb731274f56e634a7387eb94f248e19cb729ae
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-21 12:18:53 +0100

    🔧 chore: Create constant baseUrl, remove other browsers

--- apps/student/playwright.config.ts
+++ apps/student/playwright.config.ts
@@ -6,6 +6,9 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
+// Base URL for the Student Portal app.
+const baseURL = "http://localhost:3001";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -40,17 +43,17 @@ export default defineConfig({
       dependencies: ["setup"],
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-      dependencies: ["setup"],
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    //   dependencies: ["setup"],
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-      dependencies: ["setup"],
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    //   dependencies: ["setup"],
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -74,9 +77,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm dev",
+    url: baseURL,
+    reuseExistingServer: !process.env.CI,
+  },
 });
--- apps/student/tests/e2e-tests/example.spec.ts
+++ apps/student/tests/e2e-tests/example.spec.ts
@@ -2,11 +2,11 @@ import { expect, test } from "@playwright/test";
 
 test("should navigate to the about page", async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("http://localhost:3001/test");
+  await page.goto("/test");
   // Find an element with the text 'About' and click on it
   await page.click("text=About");
   // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL("http://localhost:3001/test/about");
+  await expect(page).toHaveURL("/test/about");
   // The new page should contain an h1 with "About"
   await expect(page.locator("h1")).toContainText("About");
 });
