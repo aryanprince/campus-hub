765587d93c74c696f0894ffa4a498bbdca53ac1a
Aryan Prince <aryanprincesanty@icloud.com>
2024-07-27 15:52:20 +0100

    ♻️ refactor(ui): Create `cn` utils fn using `tw-merge` and `clsx`

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,1 @@
-import type { CxOptions } from "class-variance-authority";
-import { cx } from "class-variance-authority";
-import { twMerge } from "tailwind-merge";
-
-const cn = (...inputs: CxOptions) => twMerge(cx(inputs));
-
-export { cn };
+export { cn } from "./utils/cn";
